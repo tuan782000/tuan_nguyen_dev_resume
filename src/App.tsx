1023b2f8df5a5085b24aacfe4e1d8b089837b0e1
tuan782000 <thaituan7820@gmail.com>
2023-06-27 15:25:07 +0700

    build 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Video10 from "./examples/video10/Video10";
-// import Video13 from "./examples/video13";
+import Portfolio from "./screens/portfolio";
+// import "./styles/test.scss";
+import "./styles/plugins.scss";
+import "./styles/style.scss";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
-            <h2 className="read-the-docs">
-                <Video10 
-                    defaultName={"tuan"}
-                    defaultAge={24}
-                />
-            </h2>
-        </div>
-    );
+    return <Portfolio />;
 }
 
 export default App;
